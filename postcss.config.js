252f4f28fb4a21b5c9a319eae334644eea807b56
Tilekzhann <tilekzh@gmail.com>
2025-05-13 23:51:42 +0500

    v

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
 module.exports = {
     plugins: {
-      "@tailwindcss/postcss": {},   // ← use the new package
+      tailwindcss: {},     
       autoprefixer: {},
     },
   };
